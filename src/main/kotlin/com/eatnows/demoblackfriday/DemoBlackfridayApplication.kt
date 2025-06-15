ebd191743b866d98ccbfaaa3165adbe39f5ef078
eatnows <gagyasindev@gmail.com>
2025-06-15 20:08:48 +0900

    initial commit

---
+++ src/main/kotlin/com/eatnows/demoblackfriday/DemoBlackfridayApplication.kt
@@ -0,0 +1,11 @@
+package com.eatnows.demoblackfriday
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DemoBlackfridayApplication
+
+fun main(args: Array<String>) {
+    runApplication<DemoBlackfridayApplication>(*args)
+}
---
+++ src/test/kotlin/com/eatnows/demoblackfriday/DemoBlackfridayApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.eatnows.demoblackfriday
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DemoBlackfridayApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
