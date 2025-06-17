786576eb8f25df81eb68452bf219810c840e4afd
eatnows <gagyasindev@gmail.com>
2025-06-17 23:37:21 +0900

    feat: spring kafka 의존성 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 group = "com.eatnows"
-version = "0.0.1-SNAPSHOT"
+//version = "0.0.1-SNAPSHOT"
 
 java {
     toolchain {
@@ -25,6 +25,8 @@ dependencies {
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+
+    implementation("org.springframework.kafka:spring-kafka")
 }
 
 kotlin {
